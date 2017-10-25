1bbb1898c6593ddc505230ad5d5dc17bedd656fe
Maria Clara Santana <claarasantana@gmail.com>
2017-10-25 18:44:46 -0300

    add unique key prop to items returned from map

--- src/modules/settings/SettingsView.js
+++ src/modules/settings/SettingsView.js
@@ -39,8 +39,8 @@ export default class SettingsView extends React.Component {
     const settings = [
       { name: this.props.user.get("email"), icon: ICONS.ICON_AT_BLACK }
     ];
-    const list = settings.map(setting => (
-      <ListItemWithImage text={setting.name} icon={setting.icon} />
+    const list = settings.map((index, setting) => (
+      <ListItemWithImage key={index} text={setting.name} icon={setting.icon} />
     ));
     console.log(list);
     return (
