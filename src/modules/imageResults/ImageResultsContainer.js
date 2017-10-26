bfc161c711e9a176a8bb856bfaa5fd9bbe79f525
Maria Clara Santana <claarasantana@gmail.com>
2017-10-26 19:33:53 -0300

    improve match results view

--- src/modules/imageResults/ImageResultsContainer.js
+++ src/modules/imageResults/ImageResultsContainer.js
@@ -2,5 +2,6 @@ import { connect } from "react-redux";
 import ImageResultsView from "./ImageResultsView";
 
 export default connect(state => ({
-  results: state.getIn(["snap", "results" ])
+  results: state.getIn(["snap", "results" ]),
+  challenge: state.getIn(['snap', 'currentChallenge'])
 }))(ImageResultsView);
--- src/modules/imageResults/ImageResultsView.js
+++ src/modules/imageResults/ImageResultsView.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import * as ImageBrowserState from "../imageBrowser/ImageBrowserState";
-import { View, Text, Image, TouchableHighlight, ScrollView } from "react-native";
+import { Button, View, Text, Image, TouchableHighlight, ScrollView } from "react-native";
 import Container from "../../components/Container";
 
 import styles from "./ImageResultsStyles";
@@ -14,8 +14,17 @@ export default class ImageResultsView extends React.Component {
     }
   };
 
+  goHome() {
+    this.props.navigator.push(Router.getRoute('home'));
+  }
+
+  returnToSnap() {
+    this.props.navigator.push(Router.getRoute('snap'));
+  }
+
   render() {
     const results = this.props.results.toJS();
+    const challenge = this.props.challenge.toJS()
 
     return (
       <Container>
@@ -28,12 +37,15 @@ export default class ImageResultsView extends React.Component {
                 <Image style={styles.backgroundImage} source={{ uri: results.file }} />
               </View>
               <View style={styles.resultsContainer}>
-                <Text style={styles.resultsHeadline}>Congrats, you have unlocked this challenge</Text>
+                <Text style={styles.resultsHeadline}>Congrats, you have unlocked {challenge.name}</Text>
                 <Text style={styles.resultsSubHeadline}>The following coupon code has been added to your rewards.</Text>
-                <View style={styles.codeBox}><Text style={styles.codeText}>Kd57GF</Text></View>
+                <View style={styles.codeBox}><Text style={styles.codeText}>{challenge.code}</Text></View>
                 {/*<TouchableHighlight>*/}
                   {/*<Text style={[styles.resultsSubHeadline, styles.resultsSubHeadlineLink]}>See rewards</Text>*/}
                 {/*</TouchableHighlight>*/}
+                <Button
+                  title="Go Home"
+                  onPress={() => this.goHome()} />
               </View>
             </View>
 
@@ -41,13 +53,18 @@ export default class ImageResultsView extends React.Component {
 
             <View style={styles.resultsContainer}>
 
-              <Text style={styles.resultsHeadline}>Unfortunately, there hasn't been a match</Text>
+              <Text style={styles.resultsHeadline}>Unfortunately, there hasn't been a match to {challenge.name}</Text>
               <Text style={styles.resultsSubHeadline}>Please make sure that your photo contains one of the following terms:</Text>
               {results.termsMatching && results.termsMatching.map( (term, idx) => <Text key={idx} style={styles.resultsText}>{term}</Text> )}
 
               <Text style={styles.resultsSubHeadline}>However, your photo has been annotated with the following terms:</Text>
               {results.terms && results.terms.map( (term, idx) => <Text key={idx} style={styles.resultsText}>{term}</Text> )}
-
+              <Button 
+                title="Try Again"
+                onPress={() => this.returnToSnap()}/>
+              <Button
+                title="Go Home"
+                onPress={() => this.goHome()} />
             </View>
           }
         </ScrollView>
