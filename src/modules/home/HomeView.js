833c418ced58b675d65bb5cc95aa198a1fcae102
marcelkalveram <marcel@hanno.co>
2017-10-31 11:58:52 +0100

    redo home view

--- src/modules/home/HomeView.js
+++ src/modules/home/HomeView.js
@@ -1,52 +1,43 @@
 import React from 'react'
-import { Button, Text, ScrollView, Platform, PermissionsAndroid } from 'react-native'
+import { View, Text, Image} from 'react-native'
 import Container from '../../components/Container'
+import RectButton from "../../components/RectButton";
+import * as COLORS from "../../constants/colors";
+import * as COMMON_STYLES from "../../constants/commonStyles";
+import * as ICONS from "../../constants/icons";
+
+import styles from './HomeStyles';
+
 
 export default class HomeView extends React.Component {
-  static route = {
-    navigationBar: { title: 'Home' }
-  }
 
-  componentDidMount() {
-    if (Platform.OS === 'android') {
-      return this.requestCameraPermission()
-    }
-  }
-
-  toToTabs() {
-    this.props.navigator.push(Router.getRoute('tabNavigation'))
-  }
-
-  async requestCameraPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          'title': 'Cool Photo App Camera Permission',
-          'message': 'Cool Photo App needs access to your camera ' +
-          'so you can take awesome pictures.'
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      } else {
-        console.log("Camera permission denied")
-      }
-    } catch (err) {
-      console.warn(err)
-    }
+  goToNext() {
+    this.props.navigator.push(Router.getRoute('permissions'))
   }
 
   render() {
-    return(
+    return (
       <Container>
-        <ScrollView>
-          <Text>Welcome to AdSnap</Text>
-          <Text>Finish challenges to unlock prizes.</Text>
-          <Button
-           title='Start snapping'
-           onPress={() => this.toToTabs()}
-           />
-        </ScrollView>
+        <Image style={styles.backgroundImage} source={ICONS.WELCOME_BG}>
+          <Image style={styles.logo} source={ICONS.LOGO_WHITE} />
+          <Text style={styles.description}>Here you can exchange photos of adverts for discounts from your favourite brands!</Text>
+          <View style={styles.buttonWrapper}>
+            <RectButton
+              onPress={() => this.goToNext()}
+              text={"Start snapping"}
+              width={200}
+              height={COMMON_STYLES.BUTTON_HEIGHT}
+              textColor={COLORS.WHITE}
+              backgroundColor={COLORS.TRANSPARENT}
+              borderColor={COLORS.WHITE}
+              border={{
+                borderColor: COLORS.WHITE,
+                borderStyle: "solid",
+                borderWidth: 2
+              }}
+            />
+          </View>
+        </Image>
       </Container>
     )
   }
