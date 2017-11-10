51a68205e1b098fd4f23aee61b470dcf86c90da8
Maria Clara Santana <claarasantana@gmail.com>
2017-11-09 23:14:22 -0300

    refactor presentational components into stateless ones

--- src/components/ListItemWithImage.js
+++ src/components/ListItemWithImage.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
   TouchableOpacity,
   StyleSheet,
@@ -6,38 +8,32 @@ import {
   Image,
   TextInput,
   Dimensions
-} from "react-native";
-import React from "react";
-import * as COLORS from "../constants/colors";
-export default class ListItemWithImage extends React.Component {
-  render() {
-    console.log(this.props.text);
-    return (
-      <View style={styles.container}>
-        <Image style={styles.icon} source={this.props.icon} />
-        <TextInput
-          autoCapitalize={"none"}
-          underlineColorAndroid={COLORS.TRANSPARENT}
-          style={styles.input}
-          placeholder={this.props.text}
-        />
-      </View>
-    );
-  }
-}
+} from 'react-native';
+import * as COLORS from '../constants/colors';
 
+const ListItemWithImage = () => (
+  <View style={styles.container}>
+    <Image style={styles.icon} source={props.icon} />
+    <TextInput
+      autoCapitalize={'none'}
+      underlineColorAndroid={COLORS.TRANSPARENT}
+      style={styles.input}
+      placeholder={props.text}
+    />
+  </View>
+)
 const styles = StyleSheet.create({
   icon: {
     height: 15,
     width: 15,
-    resizeMode: "contain",
+    resizeMode: 'contain',
     marginRight: 15
   },
   container: {
     marginTop: 10,
-    width: Dimensions.get("window").width - 30,
-    flexDirection: "row",
-    alignItems: "center"
+    width: Dimensions.get('window').width - 30,
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   input: {
     height: 40,
@@ -45,3 +41,10 @@ const styles = StyleSheet.create({
     color: COLORS.DARK_GREY
   }
 });
+
+ListItemWithImage.propTypes = {
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+export default ListItemWithImage
--- src/components/RectButton.js
+++ src/components/RectButton.js
@@ -1,53 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+	TouchableOpacity,
+	View,
+	Image,
+	Text
+} from 'react-native';
 import * as COLORS from '../constants/colors';
 
-import {
-    TouchableOpacity,
-    StyleSheet,
-    View,
-    Image,
-    Text
-} from 'react-native';
-
-export default class RectButton extends React.Component {
-    render() {
-        return (
-            <TouchableOpacity style={[styles.buttonWrapper(this.props.width, this.props.height), { backgroundColor: this.props.backgroundColor, borderColor: this.props.borderColor, ...this.props.border }]} onPress={() => this.props.onPress()}>
-                {this.props.text && <Text style={[styles.buttonText, { color: this.props.textColor }]}>{this.props.text}</Text>}
-            </TouchableOpacity>
-        )
-    }
-}
-
+const RectButton = (props) => (
+	<TouchableOpacity style={[
+			styles.buttonWrapper(props.width, props.height), 
+			{ backgroundColor: props.backgroundColor, 
+			borderColor: props.borderColor, 
+			...props.border }]} 
+			onPress={() => props.onPress()}>
+		{props.text && <Text style={[styles.buttonText, { color: props.textColor }]}>{props.text}</Text>}
+	</TouchableOpacity>
+)
+		
 RectButton.propTypes = {
-    onPress: React.PropTypes.func.isRequired,
-    imageUrl: React.PropTypes.string,
-    text: React.PropTypes.string,
-    width: React.PropTypes.number.isRequired,
-    heigth: React.PropTypes.number,
-    backgroundColor: React.PropTypes.string,
-    textColor: React.PropTypes.string,
-    borderColor: React.PropTypes.string,
-    border: React.PropTypes.object,
+	onPress: PropTypes.func.isRequired,
+	imageUrl: PropTypes.string,
+	text: PropTypes.string,
+	width: PropTypes.number.isRequired,
+	heigth: PropTypes.number,
+	backgroundColor: PropTypes.string,
+	textColor: PropTypes.string,
+	borderColor: PropTypes.string,
+	border: PropTypes.object,
 };
 
 const styles = {
-    buttonWrapper: (width, height) => {
-        return {
-            backgroundColor: COLORS.BUTTON_BACKGROUND,
-            width: width,
-            height: height,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderBottomWidth: 2,
-            borderRadius: 2,
-            borderColor: COLORS.BUTTON_BACKGROUND,
-        }
-    },
-    buttonText: {
-        color: COLORS.DARK_TEXT,
-        fontWeight: '500',
-        fontSize: 19,
-    }
-
+	buttonWrapper: (width, height) => {
+		return {
+			backgroundColor: COLORS.BUTTON_BACKGROUND,
+			width: width,
+			height: height,
+			justifyContent: 'center',
+			alignItems: 'center',
+			borderBottomWidth: 2,
+			borderRadius: 2,
+			borderColor: COLORS.BUTTON_BACKGROUND,
+		}
+	},
+	buttonText: {
+		color: COLORS.DARK_TEXT,
+		fontWeight: '500',
+		fontSize: 19,
+	}
 }
+
+export default RectButton
