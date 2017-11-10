3d69542771bae18352c53abb00ae4e1ad5fcaef7
Maria Clara Santana <claarasantana@gmail.com>
2017-11-09 23:37:06 -0300

    removed chalenge details

--- src/modules/challengeDetails/ChallengeDetailsContainer.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux'
-import ChallengeDetailsView from './ChallengeDetailsView'
-
-export default connect(state => ({
-  challenge: state.getIn(['challengeDetails', 'challenge']).toJS()
-}))(ChallengeDetailsView)
